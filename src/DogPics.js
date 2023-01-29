2642c80cf203ceb53777398393e04b6d925c9f88
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-01-28 19:52:49 -0500

    commit tree 4867febbb2c77f9cd796858ac653e6c2d200f749

--- src/DogPics.js
+++ src/DogPics.js
@@ -5,15 +5,21 @@ import React, { useState, useEffect } from "react";
 function DogPics() {
   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
+  useEffect(
+    // 1st arg: side effect (callback function)
+    () => console.log("useEffect called"),
+    // 2nd arg: dependencies array
+    [count]
+  );
+
+    
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  };
 
   console.log("render");
 
